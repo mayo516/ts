d9f14977080b41c968b67ad64d73855f334d25d5
eunha <dmsgk2323@naver.com>
2023-02-27 14:31:05 +0900

    인터페이스

---
+++ getting-started/3_interface.ts
@@ -0,0 +1,38 @@
+interface User {
+    age:number
+    name: string
+}
+
+let seho: User = { age: 11, name : "세호"};
+
+interface SumFunc {
+    (a:number, b: number) : number;
+}
+
+let sum : SumFunc = function (a:number, b: number) {
+    return a + b
+}
+
+//인터페이스 확장
+interface Person {
+    name: string, 
+    age: number
+}
+
+
+// interface Developer {
+//     name: string, 
+//     age: number,
+//     lang : string
+// }
+
+
+interface Developer extends Person{
+    lang : string
+}
+
+let capt : Developer = {
+    lang : "java",
+    name: "하이", 
+    age: 1,
+}
