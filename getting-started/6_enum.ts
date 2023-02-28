1ebcb36b881d7a571fcf89ff7ad6d9b900a1d015
eunha <dmsgk2323@naver.com>
2023-02-28 16:00:23 +0900

    이넘 활용

--- getting-started/6_enum.ts
+++ getting-started/6_enum.ts
@@ -13,3 +13,34 @@ let myShoes = Shoes.Nike;
 console.log(myShoes); // 1) 0  => 값을 지정하지 않으면 숫자형 이넘으로 취급됨 순서대로 0,1,2,3,4,...
 
 //2) 나이키
+
+//이넘의 활용 (예제)
+enum Answer {
+  Yes = "Y",
+  No = "N",
+}
+
+// function askQuestion(answer: string) {
+//   if (answer === "yes") {
+//     console.log("정답입니다.");
+//   }
+//   if (answer === "no") {
+//     console.log("오답입니다.");
+//   }
+// }
+
+function askQuestion(answer: Answer) {
+  if (answer === Answer.Yes) {
+    console.log("정답입니다.");
+  }
+  if (answer === Answer.No) {
+    console.log("오답입니다.");
+  }
+}
+
+askQuestion("예스");
+askQuestion(Answer.No); //이거만 됨
+askQuestion("yes");
+//셋다 되는데 여기선 안된다.
+
+//드롭다운등의 목록에서 많이 쓰인다.
