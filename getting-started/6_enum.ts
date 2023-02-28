a7d13f87f039c6a2c786400f2fc6d40460a04b08
eunha <dmsgk2323@naver.com>
2023-02-28 15:56:07 +0900

    문자형 이넘

--- getting-started/6_enum.ts
+++ getting-started/6_enum.ts
@@ -1,8 +1,15 @@
 //특정 값들의 집합을 의미하는 자료형
+// enum Shoes {
+//   Nike,
+//   Adidas,
+// }
+
 enum Shoes {
-  Nike,
-  Adidas,
+  Nike = "나이키",
+  Adidas = "아디다스",
 }
 
 let myShoes = Shoes.Nike;
-console.log(myShoes); // 0  => 값을 지정하지 않으면 숫자형 이넘으로 취급됨 순서대로 0,1,2,3,4,...
+console.log(myShoes); // 1) 0  => 값을 지정하지 않으면 숫자형 이넘으로 취급됨 순서대로 0,1,2,3,4,...
+
+//2) 나이키
