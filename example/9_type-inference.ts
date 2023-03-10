4ad12a3e5c3d00cc31482d229574711dc38ae750
eunha <dmsgk2323@naver.com>
2023-03-10 13:02:05 +0900

    제네릭 심화 타입 추론

--- example/9_type-inference.ts
+++ example/9_type-inference.ts
@@ -1,26 +1,26 @@
-var a = 'a';
+var a = "a";
 
-function logA(a = 'a') {
+function logA(a = "a") {
   var b = 10;
   return b;
 }
 
 interface Dropdown<T> {
-  value: T
+  value: T;
   title: string;
 }
 var items: Dropdown<number> = {
   value: 10,
-  title: 'a'
-}
+  title: "a",
+};
 
 interface DetailedDropdown<T> extends Dropdown<T> {
   description: string;
   tag: T;
 }
 var detailItems: DetailedDropdown<number> = {
-  value: 'hi',
-  title: 'a',
-  description: 'b',
-  tag: 'c'
-}
+  value: "hi", //Detail에서 number라고 했으니깐 이게 그대로 Dropdown으로 들어감 그래서 number여야 함
+  title: "a",
+  description: "b",
+  tag: "c",
+};
